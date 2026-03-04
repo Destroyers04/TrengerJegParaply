e6026cb5a0d81ada806506a0dcc1f564fa823ed0
Aaron21Male <aaronallezra@gmail.com>
2026-03-04 15:49:32 +0100

    Created api folder

---
+++ src/api/api.tsx
@@ -0,0 +1,10 @@
+// Link to Yr.no Nowcast api docs https://api.met.no/weatherapi/nowcast/2.0/documentation#!/data/get_complete
+// Example url:
+// https://api.met.no/weatherapi/nowcast/2.0/classic?lat=62.77354582895691&lon=8.041943076287904
+const nowcast_api_url = "https://api.met.no/weatherapi/nowcast/2.0/classic";
+
+export const get_weather_measurements = async (lat: number, lon: number) => {};
+
+// Link to Entur journeyplanner api https://developer.entur.org/pages-journeyplanner-journeyplanner
+// Example url:
+// https://api.entur.io/journey-planner/v3/graphql
