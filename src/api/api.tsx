bafe4002056ac606ee00a173726d8436d1d0be45
Destroyers04 <aaronallezra@gmail.com>
2026-03-04 21:41:39 +0100

    Created a functions to call Yr.no api

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,9 +1,16 @@
+import type { WeatherData, Coordinates } from "@/api/types";
+
 // Link to Yr.no Nowcast api docs https://api.met.no/weatherapi/nowcast/2.0/documentation#!/data/get_complete
 // Example url:
 // https://api.met.no/weatherapi/nowcast/2.0/complete?lat=62.8347&lon=8.1222
 const nowcast_api_url = "https://api.met.no/weatherapi/nowcast/2.0/complete";
 
-export const get_weather_measurements = async (lat: number, lon: number) => {};
+// Lat and Lon should have max 4 decimals
+export const get_weather_measurements = async ({lat, lon}:Coordinates):Promise<WeatherData>=> {
+    // My custom header is 
+    const response = await fetch(`${nowcast_api_url}?lat=${lat}&lon=${lon}`) 
+    return await response.json()
+};
 
 // Link to Entur journeyplanner api https://developer.entur.org/pages-journeyplanner-journeyplanner
 // Example url:
