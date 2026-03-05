970c6ee58d92712f57064d1ec7e2e9fcf5858531
Aaron21Male <aaronallezra@gmail.com>
2026-03-05 16:57:09 +0100

    Tested newly created api calls

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,24 @@
-import { Button } from "@/components/ui/button";
 import {
-  Card,
-  CardAction,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { get_weather_measurements } from "@/api/api";
+  get_bus_stop_data,
+  get_route_details_data,
+  get_weather_measurements_data,
+} from "@/api/api";
 import { useEffect } from "react";
+import { TripForm } from "@/components/TripForm/trip-form";
 
 function App() {
-  useEffect(() => {
-    get_weather_measurements({ lat: 62.5, lon: 8.111 })
-      .then(data => console.log(data))
-  }, [])
+/*   useEffect(() => {
+    get_weather_measurements_data({ lat: 62.5, lon: 8.111 }).then((data) =>
+      console.log(data),
+    );
+    get_bus_stop_data({
+      user_input_address: "rødstrandvegen 132, Molde",
+    }).then((data) => console.log(data));
+  }, []); */
   return (
-    <>
-      
-    </>
+    <div className="max-w-screen-xl mx-auto mt-8 flex justify-center">
+      <TripForm />
+    </div>
   );
 }
 
