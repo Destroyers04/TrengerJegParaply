33ca02ff32d7ddce1696451c5e4368e51a560949
Aaron21Male <aaronallezra@gmail.com>
2026-03-06 13:17:50 +0100

    Made handle click change status

--- src/components/TripForm/trip-form.tsx
+++ src/components/TripForm/trip-form.tsx
@@ -13,24 +13,26 @@ import {
   get_route_details_data,
   get_weather_measurements_data,
 } from "@/api/api";
-import type {
-  GeocoderFeature,
-  Coordinates,
-  JourneyPlannerParam,
-} from "@/api/types";
+import type { GeocoderFeature, Coordinates } from "@/api/types";
 
 interface locationProps {
   from: string;
   to: string;
 }
 
-export function TripForm() {
+interface TripFormProps {
+  status: boolean;
+  setStatus: (value: boolean) => void;
+}
+
+export function TripForm({ status, setStatus }: TripFormProps) {
   const [from, setFrom] = useState("");
   const [to, setTo] = useState("");
 
   const [, formAction, isPending] = useActionState(submitInputData, null);
 
   const handleClick = async ({ from, to }: locationProps) => {
+    setStatus(true);
     const fromLocation = JSON.parse(from) as GeocoderFeature;
     const toLocation = JSON.parse(to) as GeocoderFeature;
 
@@ -54,16 +56,12 @@ export function TripForm() {
         dateTime: currentTime.toISOString(),
       }),
     ]);
-    console.log(weatherFrom);
-    console.log(weatherTo);
-    console.log(route_details);
   };
 
   async function submitInputData(_previousState: null, formData: FormData) {
     try {
       const from = formData.get("from") as string;
       const to = formData.get("to") as string;
-      console.log(from, to);
       await handleClick({ from, to });
     } catch (error) {
       return null;
@@ -71,36 +69,34 @@ export function TripForm() {
     return null;
   }
   return (
-    <div className="w-full max-w-md border-solid border-4 p-8 rounded-md">
-      <form action={formAction}>
-        <FieldGroup>
-          <FieldSet>
-            <FieldLegend>Trenger du paraply? ☂️</FieldLegend>
-            <FieldDescription>Velg hvor du reiser fra og til</FieldDescription>
-            <FieldGroup className="gap-3">
-              <Field>
-                <FieldLabel>Fra</FieldLabel>
-                <InputField
-                  name="from"
-                  required
-                  value={from}
-                  setValue={setFrom}
-                />
-              </Field>
-              <Field>
-                <FieldLabel>Til</FieldLabel>
-                <InputField name="to" required value={to} setValue={setTo} />
-              </Field>
-            </FieldGroup>
-          </FieldSet>
+    <form action={formAction}>
+      <FieldGroup>
+        <FieldSet>
+          <FieldLegend>Trenger du paraply? ☂️</FieldLegend>
+          <FieldDescription>Velg hvor du reiser fra og til</FieldDescription>
+          <FieldGroup className="gap-3">
+            <Field>
+              <FieldLabel>Fra</FieldLabel>
+              <InputField
+                name="from"
+                required
+                value={from}
+                setValue={setFrom}
+              />
+            </Field>
+            <Field>
+              <FieldLabel>Til</FieldLabel>
+              <InputField name="to" required value={to} setValue={setTo} />
+            </Field>
+          </FieldGroup>
+        </FieldSet>
 
-          <Field orientation="vertical">
-            <Button type="submit">
-              {isPending ? "Søker etter ruter" : "Sjekk nå"}
-            </Button>
-          </Field>
-        </FieldGroup>
-      </form>
-    </div>
+        <Field orientation="vertical">
+          <Button type="submit">
+            {isPending ? "Søker etter ruter" : "Sjekk nå"}
+          </Button>
+        </Field>
+      </FieldGroup>
+    </form>
   );
 }
