89a72341641dba9d504b9549d6ae1c2ddbb110b8
Aaron21Male <aaronallezra@gmail.com>
2026-03-09 14:53:00 +0100

    Added rain check, wind check and final verdict

--- src/components/Result/result-verdict.tsx
+++ src/components/Result/result-verdict.tsx
@@ -1,11 +1,13 @@
 import type { TripResults } from "@/components/TripForm/trip-form";
-import { useEffect } from "react";
 
 export function ResultVerdict({ results }: { results: TripResults | null }) {
+  // Null check
   if (!results) return null;
+
   const entry_from = results.weatherFrom.properties.timeseries[0].data;
   const entry_to = results.weatherTo.properties.timeseries[0].data;
 
+  // Returnerer true hvis det er mye regn, ellers returner false
   const checkRain = () => {
     const precipitation_rate_from =
       entry_from.instant.details.precipitation_rate;
@@ -15,32 +17,46 @@ export function ResultVerdict({ results }: { results: TripResults | null }) {
     const precipitation_rate_to = entry_to.instant.details.precipitation_rate;
     const precipitation_amount_to =
       entry_to.next_1_hours?.details.precipitation_amount;
-    entry_to.next_1_hours?.details.precipitation_amount;
 
     if (precipitation_amount_to === 0.0 || precipitation_amount_from === 0.0) {
       return false;
     }
     // Wikipedia (https://en.wikipedia.org/wiki/Precipitation_types) sier at moderat regn starter på 2.6mm
-    if (precipitation_rate_from < 2.6 || precipitation_rate_to < 2.6) {
-      return false;
+    if (precipitation_rate_from >= 2.6 || precipitation_rate_to >= 2.6) {
+      return true;
     }
-    return true;
+    return false;
   };
+
+  // Returnerer true hvis det er mye vind, ellers returner false
   const checkWind = () => {
     const wind_gust_from = entry_from.instant.details.wind_speed_of_gust;
     const wind_gust_to = entry_to.instant.details.wind_speed_of_gust;
-    // følger beaufort skalaen (https://no.wikipedia.org/wiki/Beauforts_skala) som sier at paraply blir vanskelig å bruke ved liten kuling (10.8)
+
+    // Vind hastighet er ikke et obligatorisk måling, vi må derfor sjekke
+    if (wind_gust_from === undefined || wind_gust_to === undefined)
+      return undefined;
+
+    // følger beaufort skalaen (https://no.wikipedia.org/wiki/Beauforts_skala) som sier at paraply blir vanskelig å bruke ved liten kuling (10.8m/s)
+    if (wind_gust_from >= 10.8 || wind_gust_to >= 10.8) {
+      return true;
+    }
+    return false;
   };
 
   const verdict = () => {
     if (!checkRain()) {
-      return "Du trenger ikke paraply i dag!";
+      return "Du trenger ikke paraply!";
     }
+    if (checkWind() === undefined) {
+      return "Paraply anbefales, men vi vet dessverre ikke om det blåser";
+    }
+    if (!checkWind()) {
+      return "Paraply anbefales!";
+    }
+
+    return "Vi anbefaler ikke paraply, men vi anbefaler regnjakke!";
   };
 
-  return (
-    <div className="max-w-screen-xl flex justify-center items-center h-screen pt-8 border-solid border-4">
-      {verdict()}
-    </div>
-  );
+  return <div>{verdict()}</div>;
 }
