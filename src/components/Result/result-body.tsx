149a1c96664c71676fd3e107c44f38bb667dde8e
Aaron21Male <aaronallezra@gmail.com>
2026-03-09 15:18:30 +0100

    Styling

--- src/components/Result/result-body.tsx
+++ src/components/Result/result-body.tsx
@@ -4,9 +4,9 @@ import type { TripResults } from "@/components/TripForm/trip-form";
 
 export function ResultBody({ results }: { results: TripResults | null }) {
   return (
-    <div className="max-w-screen-xl mx-auto flex justify-center items-center h-screen pt-8 ">
+    <div className="max-w-screen-xl mx-auto flex flex-col justify-center items-center h-screen pt-8 gap-12">
       <ResultVerdict results={results} />
-      <ProgressBar />
+      <ProgressBar results={results} />
     </div>
   );
 }
